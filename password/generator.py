94c6d956235ac2be8f4a7331a6a6590057c58f31
mamaz <mamaz@kata.ai>
2020-04-04 18:10:54 +0700

    Make password package

--- password/generator.py
+++ password/generator.py
@@ -1,8 +1,12 @@
 import random
+import sys
+from pathlib import Path
 from typing import List
 
 def generate_password(length: int = 4) -> str:
-    words = read_all("./common_words_en.txt")
+    path = str(Path(__file__).parent) + "/common_words_en.txt"
+    words = read_all(path)
+
     password = ""
     for i in range(0, length):
         password += random.choice(words) + " "
@@ -18,10 +22,9 @@ def read_all(file_path: str) -> List[str]:
             while line:
                 result.append(line.replace("\n", ""))
                 line = file.readline()
+        return result
     except:
-        print("error read")
-
-    return result
+        print("error read file", sys.exc_info()[0])
 
 if __name__ == "__main__":
     print(generate_password())
